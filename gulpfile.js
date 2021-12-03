b5d441b96725c2529fc2a9d60a8da225537061a5
Tzuhui <angel1996421@gmail.com>
2021-12-03 16:35:44 +0800

    Update Detail

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 const gulp = require('gulp');
-const merge = require('gulp-merge-json');
 const browserSync = require('browser-sync');
+const jsonSchema = require("gulp-json-schema");
+const merge = require('gulp-merge-json');
 const ejs = require("gulp-ejs");
 const rename = require('gulp-rename');
+const deploy = require('gulp-gh-pages');
  
+function checkJsonSchema(done) {
+  return gulp.src("jsonFiles/*.json")
+    .pipe(jsonSchema("./schema.json"))
+    .on('error', done);
+}
+function checkJsonSchemaAll(done) {
+  return gulp.src("dist/*.json")
+    .pipe(jsonSchema("./schemaAll.json"))
+    .on('error', done);
+}
 function mergeGulpFiles() {
   return gulp.src('jsonFiles/*.json')
     .pipe(merge())
@@ -28,8 +40,14 @@ function browserSyncReload(done) {
   done();
 }
 
+function deploy() {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+}
+
 function watch() {
   return gulp.watch(['source/*.ejs', 'source/**/*.ejs']).on('change', gulp.series(parseEjs, browserSyncReload));
 }
-exports.default = gulp.parallel(mergeGulpFiles, parseEjs, browser, watch)
-exports.build = gulp.parallel(mergeGulpFiles, parseEjs)
+exports.default = gulp.series(checkJsonSchema, mergeGulpFiles, parseEjs, browser, watch)
+exports.build = gulp.parallel(mergeGulpFiles, parseEjs, deploy)
+exports.check = gulp.parallel(checkJsonSchema)
